8d5a24e0f56630e751bd032c7b16f1500fe2adba
Gleb <gleb.gadbrat@mail.ru>
2022-06-03 13:11:59 +0300

    Задание 3

--- dz.py
+++ dz.py
@@ -12,13 +12,51 @@
     
 # print(get_up(nums))
 
-from random import randint
+# Задание 2 Создать и заполнить файл случайными целыми значениями. 
+# Выполнить сортировку содержимого файла по возрастанию.
 
-with open('file.txt','w') as data:
-    for i in range (randint(1,14)): 
-        data.write(str(randint(1,1000)) + ',')
+# from random import randint
 
-with open('file.txt','r') as data2:
-    a=data2.readline().split(',')
-    a.pop()
-    print(sorted(map(int, a)))
+# with open('file.txt','w') as data:
+#     for i in range (randint(1,14)): 
+#         data.write(str(randint(1,1000)) + ',')
+
+# with open('file.txt','r') as data2:
+#     a=data2.readline().split(',')
+#     a.pop()
+#     print(sorted(map(int, a)))
+
+# Задание 3  найти триплеты и просто выводить их на экран. 
+# Триплетом называются три числа, которые в сумме дают 0. 
+# (решение будет долгим, ибо является демонстрационным при теме 
+# многопоточного программирования).
+
+from dataclasses import replace
+from re import A
+
+
+def findTriplets(arr, n):
+    found = False
+    for i in range(0, n-2):
+        for j in range(i+1, n-1):
+            for k in range(j+1, n):
+                if (arr[i] + arr[j] + arr[k] == 0):
+                    print(arr[i], arr[j], arr[k])
+                    found = True   
+    if (found == False):
+        print("Триплеты не найдены")
+def concatenation(*params):
+        res: str = ""
+        for item in params:
+            res+= item
+        return res
+
+chisla = []
+with open('file.txt', 'r') as data:
+    for line in data:
+        line = concatenation(line)
+        line.replace('\n','')
+        line.replace(' ', '')
+        chisla.append(int(line))
+n = len(chisla)
+findTriplets(chisla, n)
