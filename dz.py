ae131b47c190f0f2d977c93edd2d363c685fc0af
Gleb <gleb.gadbrat@mail.ru>
2022-06-03 12:37:43 +0300

    Задание 2

--- dz.py
+++ dz.py
@@ -1,15 +1,24 @@
 # Задание 1 Дан список чисел. Создать список, в который попадают числа, 
 # описываемые возрастающую последовательность.
 
-nums = [3, 1, 2, 3, 4, 6, 1, 7]
+# nums = [3, 1, 2, 3, 4, 6, 1, 7]
 
-# Первый вариант
-
-def get_up(nums):
-    next = [nums[0]]
-    for i in nums:
-        if i > max(next):
-            next.append(i)
-    return next
+# def get_up(nums):
+#     next = [nums[0]]
+#     for i in nums:
+#         if i > max(next):
+#             next.append(i)
+#     return next
     
-print(get_up(nums))
+# print(get_up(nums))
+
+from random import randint
+
+with open('file.txt','w') as data:
+    for i in range (randint(1,14)): 
+        data.write(str(randint(1,1000)) + ',')
+
+with open('file.txt','r') as data2:
+    a=data2.readline().split(',')
+    a.pop()
+    print(sorted(map(int, a)))
